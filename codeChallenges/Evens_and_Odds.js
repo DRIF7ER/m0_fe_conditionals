b3b28d42c6a606709f4451e6d0fed73b4b3530de
Charles B. <charlesbanks238@gmail.com>
2024-03-10 03:51:33 -0700

    Adds the functional code for even and odd operation

---
+++ codeChallenges/Evens_and_Odds.js
@@ -0,0 +1,10 @@
+/** The remainder (%) operator returns the remainder left over when one operand is divided by a second operand.
+ * It always takes the sign of the dividend. In my own words I believe this is saying that two numbers (in this case) fit equally into this
+ * other number. When they don't, it's simply saying, 'hey, there's extra number left over, over here'.
+ * Syntax (x % y)
+ */
+
+var numQuestion = 1000
+
+if (numQuestion % 2 === 0){console.log("even")
+} else{console.log("odd")}
